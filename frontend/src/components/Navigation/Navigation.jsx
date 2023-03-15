e4649ce2d9c964d7932d9cdf15fe74328e108606
kevin munoz <harquin09@gmail.com>
2023-03-14 22:16:47 -0500

    icons

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import avatar from "../../img/avatar.png ";
+import { menuItems } from "../../utils/menuItems";
 
 const Navigation = () => {
-  return <NavStyled>Navigation</NavStyled>;
+  return (
+    <NavStyled>
+      <div className="user-con">
+        <img src={avatar} alt="" />
+        <div className="text">
+          <h2>VinKE</h2>
+          <p>Tu Dinero</p>
+        </div>
+      </div>
+      <ul className="menu-items">
+        {menuItems.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.icon}
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="bottom-nav">
+        <li></li>
+      </div>
+    </NavStyled>
+  );
 };
 
 const NavStyled = styled.nav``;
